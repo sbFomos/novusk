edb0cd3da95f36b8c65dbd2514a96a710761e850
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-01-22 18:58:44 -0500

    Made simple os for the kernel.

--- os/src/lib.rs
+++ os/src/lib.rs
@@ -1,11 +1,16 @@
 #![no_std]
 
 extern crate arch;
+use arch::x86::kernel::vga_buffer::Color::*;
 extern crate include;
+use include::{time::{sleep}};
 
 #[macro_use]
 extern crate novusk_lib;
+use novusk_lib::ui;
 
 pub fn main() {
-
+    println!("Starting example os...");
+    sleep(1);
+    ui::display::fill_screen(Cyan);
 }
